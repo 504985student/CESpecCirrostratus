098b24bec8a2368e1af0c88ef43322a8550b8e0b
TinekeS <504985@student.saxion.nl>
2024-11-01 15:39:40 +0100

    fixed instanceprofile

--- Cloud_Orchestration/variables.tf
+++ Cloud_Orchestration/variables.tf
@@ -1,6 +1,6 @@
 variable "shared_credentials_file" {
     #description = "~/.aws/credentials"
-    default = "C:\\Users\\<user>\\.aws\\credentials"
+    default = "C:\\Users\\Ash\\.aws\\credentials"
 }
 variable "region" {
   default = "us-east-1"
