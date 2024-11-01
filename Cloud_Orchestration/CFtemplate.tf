c531e9a6b91ecc87902136c59f852cfa8c382b65
TinekeS <504985@student.saxion.nl>
2024-11-01 15:38:03 +0100

    added terraform for aws

---
+++ Cloud_Orchestration/CFtemplate.tf
@@ -0,0 +1,16 @@
+resource "aws_cloudformation_stack" "CONetwork" {
+    name = "CONetwork"
+    template_body = file("${path.module}/CO_Network.yml")
+}
+
+resource "aws_cloudformation_stack" "CO_ASG" {
+    name = "CO_ASG"
+    depends_on = [aws_cloudformation_stack.CONetwerk]
+    template_body = file("${path.module}/CO_ASG.yml")
+}
+
+resource "aws_cloudformation_stack" "COSwarmMaster" {
+    name = "COSwarmMaster"
+    depends_on = [aws_cloudformation_stack.CONetwork]
+    template_body = file("${path.module}/CO_SwarmMaster.yml")
+}
