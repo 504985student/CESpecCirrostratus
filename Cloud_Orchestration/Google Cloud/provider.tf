ab466fdda5e1e518eb3ba0ee266c2ce857d83812
TinekeS <504985@student.saxion.nl>
2024-11-02 19:40:25 +0100

    added docker compose to userdata

--- Cloud_Orchestration/Google Cloud/provider.tf
+++ Cloud_Orchestration/Google Cloud/provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-    credentials = file("/<credentials>.json")
-    project     = "<project>"
+    credentials = file("/credentials.json")
+    project     = "smart-seer-434509-i6"
     region      = "europe-west1"
 }
